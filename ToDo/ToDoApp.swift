20785c874cd1bf81f220fb4c53601e69af65dd7c
Jose Correa <joseecorrea06@gmail.com>
2021-07-13 21:12:53 +0100

    App initial commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Jose E on 13/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
